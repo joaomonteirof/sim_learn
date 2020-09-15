64fcbeb9dd98442119bf1c69a642201b3be7e5c0
joaomonteirof <joaomonteirof@gmail.com>
2020-09-15 10:38:11 -0400

    new lr schedule

--- imagenet/train_loop.py
+++ imagenet/train_loop.py
@@ -28,7 +28,7 @@ class TrainLoop(object):
 		self.ablation_ce = ablation_ce
 		self.model = model
 		self.optimizer = optimizer
-		self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[10, 30] if pretrained else [10, 30, 60, 90], gamma=0.1)
+		self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[10, 30] if pretrained else [8, 30, 60, 90], gamma=0.1)
 		self.max_gnorm = max_gnorm
 		self.train_loader = train_loader
 		self.valid_loader = valid_loader
