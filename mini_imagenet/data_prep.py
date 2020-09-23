0a2bdb0aa6e3b10a58c8f33d7522d9158322ac25
joaomonteirof <joaomonteirof@gmail.com>
2020-09-22 20:53:02 -0400

    option for including extra data into the same hdf

--- mini_imagenet/data_prep.py
+++ mini_imagenet/data_prep.py
@@ -10,7 +10,8 @@ from tqdm import tqdm
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='Train data preparation and storage in .hdf')
-	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to scp files with features')
+	parser.add_argument('--path-to-data', type=str, default='./data/', metavar='Path', help='Path to root folder with sub-folders containing files')
+	parser.add_argument('--path-to-more-data', type=str, default=None, metavar='Path', help='Path to root folder with sub-folders containing files')
 	parser.add_argument('--out-path', type=str, default='./', metavar='Path', help='Path to output hdf file')
 	parser.add_argument('--out-name', type=str, default='train.hdf', metavar='Path', help='Output hdf file name')
 	parser.add_argument('--n-workers', type=int, default=4, metavar='N', help='Workers for data loading. Default is 4')
@@ -24,13 +25,16 @@ if __name__ == '__main__':
 	dataset = datasets.ImageFolder(args.path_to_data, transform=transform)
 	dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=args.n_workers)
 
+	if args.path_to_more_data is not None:
+		extra_dataset = datasets.ImageFolder(args.path_to_more_data, transform=transform)
+	else:
+		extra_dataset = None
+
 	print('Start of data preparation')
 
 	hdf = h5py.File(args.out_path+args.out_name, 'a')
 
-	data_iterator = tqdm(enumerate(dataloader), total=len(dataloader))
-
-	for i, data_label in data_iterator:
+	for i, data_label in enumerate(dataloader):
 
 		x, y = data_label
 		y = str(y.squeeze().item())
@@ -39,5 +43,20 @@ if __name__ == '__main__':
 			hdf.create_group(y)
 
 		hdf[y].create_dataset(y+'_'+str(i), data=x.squeeze(0))
+
+	if extra_dataset is not None:
+
+		extra_dataloader = torch.utils.data.DataLoader(extra_dataset, batch_size=1, shuffle=False, num_workers=args.n_workers)
+
+		for j, data_label in enumerate(extra_dataloader):
+
+			x, y = data_label
+			y = str(y.squeeze().item())
+
+			if not y in hdf:
+				hdf.create_group(y)
+
+			hdf[y].create_dataset(y+'_'+str(i+j+1), data=x.squeeze(0))
+
 
 	hdf.close()
