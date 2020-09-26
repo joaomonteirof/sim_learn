a280cb6df91cb680675697f4cf51002880a6a592
joaomonteirof <joaomonteirof@gmail.com>
2020-09-26 12:36:59 -0400

    rolling back to sgd

--- retrieval/train.py
+++ retrieval/train.py
@@ -21,11 +21,10 @@ parser = argparse.ArgumentParser(description='Image retrieval')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
 parser.add_argument('--valid-batch-size', type=int, default=16, metavar='N', help='input batch size for testing (default: 256)')
 parser.add_argument('--epochs', type=int, default=500, metavar='N', help='number of epochs to train (default: 500)')
-parser.add_argument('--lr', type=float, default=0.001, metavar='LR', help='learning rate (default: 0.001)')
+parser.add_argument('--lr', type=float, default=0.0001, metavar='LR', help='learning rate (default: 0.0001)')
 parser.add_argument('--lr-steps', nargs='+', type=int, default=[20,50,120], help='List of epochs to reduce lr by lr-factor')
 parser.add_argument('--lr-factor', type=float, default=0.1, metavar='LR', help='Factor to reduce learning rate (default: 0.1)')
-parser.add_argument('--beta1', type=float, default=0.9, metavar='beta1', help='Beta1 (default: 0.9)')
-parser.add_argument('--beta2', type=float, default=0.999, metavar='beta2', help='Beta2 (default: 0.9)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='momentum', help='Momentum (default: 0.9)')
 parser.add_argument('--l2', type=float, default=1e-4, metavar='lambda', help='L2 wheight decay coefficient (default: 0.0005)')
 parser.add_argument('--smoothing', type=float, default=0.2, metavar='l', help='Label smoothing (default: 0.2)')
 parser.add_argument('--centroid-smoothing', type=float, default=0.9, metavar='Lamb', help='Moving average parameter for centroids')
@@ -128,7 +127,7 @@ if args.logdir:
 else:
 	writer = None
 
-optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2), weight_decay=args.l2)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=momentum, weight_decay=args.l2)
 
 trainer = TrainLoop(model, optimizer, train_loader, valid_loader, max_gnorm=args.max_gnorm,
 		lr_steps=args.lr_steps, lr_factor=args.lr_factor, label_smoothing=args.smoothing, verbose=args.verbose, 
