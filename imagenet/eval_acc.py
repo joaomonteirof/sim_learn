7e08ae93e2afdfaf5a53ed3548f89f52baf2d549
joaomonteirof <joaomonteirof@gmail.com>
2020-09-27 14:32:03 -0400

    minor fix on eval

--- imagenet/eval_acc.py
+++ imagenet/eval_acc.py
@@ -49,13 +49,17 @@ if __name__ == '__main__':
 	else:
 		device = torch.device('cpu')
 
+	if args.cuda:
+		device = get_freer_gpu()
+	else:
+		device = torch.device('cpu')
+
 	model = model.to(device)
 	model.centroids = model.centroids.to(device)
 
 	model.eval()
 
-	correct_ce_1, correct_ce_5, correct_sim_1, correct_sim_5, correct_mix_1, correct_mix_5 = 0, 0, 0, 0, 0, 0
-	total = 0
+	correct_ce, correct_sim, correct_mix = 0, 0, 0
 
 	with torch.no_grad():
 
@@ -65,31 +69,22 @@ if __name__ == '__main__':
 			x, y = batch
 
 			x = x.to(device)
-			y = y.to(device)
+			y = y.to(device).squeeze()
 
 			embeddings = model.forward(x)
 
-			pred_ce = F.softmax(model.out_proj(embeddings), dim=1)
-			(correct_ce_1_, correct_ce_5_) = correct_topk(pred_ce, y, (1,5))
+			out_ce = F.softmax(model.out_proj(embeddings), dim=1)
+			pred_ce = out_ce.max(1)[1].long()
+			correct_ce += pred_ce.squeeze().eq(y).sum().item()
 
-			pred_sim = F.softmax(model.compute_logits(embeddings, ablation=args.ablation_sim), dim=1)
-			(correct_sim_1_, correct_sim_5_) = correct_topk(pred_sim, y, (1,5))
+			out_sim = F.softmax(model.compute_logits(embeddings, ablation=args.ablation_sim), dim=1)
+			pred_sim = out_sim.max(1)[1].long()
+			correct_sim += pred_sim.squeeze().eq(y).sum().item()
 
-			pred_mix = 0.5*pred_ce+0.5*pred_sim
-			(correct_mix_1_, correct_mix_5_) = correct_topk(pred_mix, y, (1,5))
+			out_mix = 0.5*out_ce+0.5*out_sim
+			pred_mix = out_mix.max(1)[1].long()
+			correct_mix += pred_mix.squeeze().eq(y).sum().item()
 
-			correct_ce_1 += correct_ce_1_
-			correct_ce_5 += correct_ce_5_
-			correct_sim_1 += correct_sim_1_
-			correct_sim_5 += correct_sim_5_
-			correct_mix_1 += correct_mix_1_
-			correct_mix_5 += correct_mix_5_
-			total += x.size(0)
-
-	print('\nCE Top 1 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_ce_1)/total))
-	print('\nCE Top 5 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_ce_5)/total))
-	print('\nSIM Top 1 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_sim_1)/total))
-	print('\nSIM Top 5 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_sim_5)/total))
-	print('\nMIX Top 1 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_mix_1)/total))
-	print('\nMIX Top 5 Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*float(correct_mix_5)/total))
-
+	print('\nCE Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*correct_ce/len(testset)))
+	print('\nSIM Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*correct_sim/len(testset)))
+	print('\nMIX Accuracy of model {}: {}\n'.format(args.cp_path.split('/')[-1], 100.*correct_mix/len(testset)))
