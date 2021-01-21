0a23ffe8cf489350b13e2f7006808aba7cd7e578
joaomonteirof <joaomonteirof@gmail.com>
2021-01-21 10:46:50 -0500

    adding scripts to plot histograms of scores properties

--- cifar/plot_tsne.py
+++ cifar/plot_tsne.py
@@ -4,7 +4,7 @@ import torch
 from train_loop import TrainLoop
 import torch.optim as optim
 from torchvision import datasets, transforms
-from models import resnet, wideresnet
+from models import resnet, wideresnet, wrapper_racc
 import numpy as np
 import os
 import sys
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 	parser.add_argument('--cp-path', type=str, default=None, metavar='Path', help='Path for checkpointing')
 	parser.add_argument('--data-path', type=str, default='./data/', metavar='Path', help='Path to data')
 	parser.add_argument('--model', choices=['resnet', 'wideresnet'], default='resnet')
+	parser.add_argument('--inf-mode', choices=['sim', 'ce', 'fus'], default='sim', help='Inference mode')
 	parser.add_argument('--out-path', type=str, default='', metavar='Path', help='Path for saving outputs')
 	parser.add_argument('--out-prefix', type=str, default='', metavar='Path', help='Prefix to be added to output file name')
 	parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables GPU use')
@@ -27,7 +28,7 @@ if __name__ == '__main__':
 
 	transform_test = transforms.Compose([transforms.ToTensor(), transforms.Normalize([x / 255 for x in [125.3, 123.0, 113.9]], [x / 255 for x in [63.0, 62.1, 66.7]])])
 	validset = datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform_test)
-	labels_list = [x[1] for x in validset]
+	preprocessing = dict(mean=[x / 255 for x in [125.3, 123.0, 113.9]], std=[x / 255 for x in [63.0, 62.1, 66.7]], axis=-3)
 
 	ckpt = torch.load(args.cp_path, map_location = lambda storage, loc: storage)
 	try :
@@ -57,64 +58,37 @@ if __name__ == '__main__':
 	model = model.to(device)
 	model.centroids = model.centroids.to(device)
 
-	idxs_enroll, idxs_test, labels = create_trials_labels(labels_list)
-	print('\n{} trials created out of which {} are target trials'.format(len(idxs_enroll), np.sum(labels)))
+	model = wrapper_racc.wrapper(base_model=model, inf_mode=args.inf_mode)
 
-	target_scores_e2e = []
-	non_target_scores_e2e = []
-	target_scores_cos = []
-	non_target_scores_cos = []
+	model.eval()
 
-	mem_embeddings = {}
+	fmodel = PyTorchModel(model, bounds=(0, 1), preprocessing=preprocessing, device=device)
+
+	clean_embeddings, attack_embeddings = [], []
 
 	model.eval()
 
 	with torch.no_grad():
 
-		iterator = tqdm(range(len(labels)), total=len(labels))
+		iterator = tqdm(range(len(validset)), total=len(validset))
 		for i in iterator:
 
-			enroll_ex = str(idxs_enroll[i])
+			enroll_ex_data = validset[i][0].unsqueeze(0)
 
-			try:
-				emb_enroll = mem_embeddings[enroll_ex]
-			except KeyError:
+			if args.cuda:
+				enroll_ex_data = enroll_ex_data.cuda(device)
 
-				enroll_ex_data = validset[idxs_enroll[i]][0].unsqueeze(0)
+			emb_enroll = model.forward(enroll_ex_data).detach()
 
-				if args.cuda:
-					enroll_ex_data = enroll_ex_data.cuda(device)
-
-				emb_enroll = model.forward(enroll_ex_data).detach()
-				mem_embeddings[str(idxs_enroll[i])] = emb_enroll
-
-			test_ex = str(idxs_test[i])
-
-			try:
-				emb_test = mem_embeddings[test_ex]
-			except KeyError:
-
-				test_ex_data = validset[idxs_test[i]][0].unsqueeze(0)
-
-				if args.cuda:
-					test_ex_data = test_ex_data.cuda(device)
-
-				emb_test = model.forward(test_ex_data).detach()
-				mem_embeddings[str(idxs_test[i])] = emb_test
-
-			e2e_score = torch.sigmoid(model.forward_bin(emb_enroll, emb_test)).squeeze().item()
-			cos_score = torch.nn.functional.cosine_similarity(emb_enroll, emb_test).squeeze().item()
-	
-			if labels[i] == 1:
-				target_scores_e2e.append(e2e_score)
-				target_scores_cos.append(cos_score)
-			elif labels[i] == 2:
-				non_target_scores_e2e.append(e2e_score)
-				non_target_scores_cos.append(cos_score)
-			else:
-				print(f'Unknown label: {label}')
+			scores_dif.append( 1.-torch.sigmoid(model.forward_bin(emb_enroll, emb_enroll)).squeeze().item() )
 
 	print('\nScoring done')
+
+	print('Avg: {}'.format(np.mean(scores_dif)))
+	print('Std: {}'.format(np.std(scores_dif)))
+	print('Median: {}'.format(np.median(scores_dif)))
+	print('Max: {}'.format(np.max(scores_dif)))
+	print('Min: {}'.format(np.min(scores_dif)))
 
 	if not args.no_histogram:
 		import matplotlib
@@ -122,7 +96,5 @@ if __name__ == '__main__':
 		matplotlib.rcParams['ps.fonttype'] = 42
 		matplotlib.use('agg')
 		import matplotlib.pyplot as plt
-		plt.hist((target_scores_e2e, non_target_scores_e2e), density=True, bins=30, color=('blue', 'red'))
-		plt.savefig(os.path.join(args.out_path, args.out_prefix, 'e2e_scores_hist_cifar.pdf'), bbox_inches='tight')
-		plt.hist((target_scores_cos, non_target_scores_cos), density=True, bins=30, color=('blue', 'red'))
-		plt.savefig(os.path.join(args.out_path, args.out_prefix, 'cos_scores_hist_cifar.pdf'), bbox_inches='tight')
+		plt.hist(scores_dif, density=True, bins=30)
+		plt.savefig(os.path.join(args.out_path, args.out_prefix, 'met_hist_cifar.pdf'), bbox_inches='tight')
